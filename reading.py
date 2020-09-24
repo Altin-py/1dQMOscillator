85c9c6d97baa779a2d829d478077aa24dfa20aec
Altin Shala <s_b6kpo6@uni-bremen.de>
2020-09-24 13:19:59 +0200

    Converted the interpolation data into matrix

--- reading.py
+++ reading.py
@@ -6,7 +6,9 @@ converts input potential data into a plottable
 file of the interpolated potential.
 """
 import os.path
-def readfile(directory):
+import numpy as np
+# import numpy as np
+def reading(directory):
     """
     Reads input potentialextracts variables
 
@@ -23,10 +25,18 @@ def readfile(directory):
     with open(file) as fp:
         data = fp.read().splitlines()
     mass = data[0]
-    diskr = data[1]      #array mit Daten für Diskret
-    eigv = data[2]      #first and last eigv
+    mass = np.asfarray(mass, float)
+    diskr = data[1].split()     #array mit Daten für Diskret
+    diskr = np.asfarray(diskr, float)
+    eigv = data[2].split()      #first and last eigv
+    eigv = np.asfarray(eigv, float)
     ansatz = data[3]    #Interpolationsansatz
-    numb = data[4][0]      #anzahl interpolationspunkte
+    numb = data[4].split()      #anzahl interpolationspunkte
+    numb = numb[0]
     numb = int(numb)
-    inpo = data[5:5+numb-1]
-    return mass, diskr, eigv, ansatz, numb, inpo
+    inpo = data[5:5+numb]
+    matinpo = np.zeros((numb, 2))
+    for ii in range(numb):
+        for jj in range(2):
+            matinpo[ii, jj] = np.float(inpo[ii].split()[jj])
+    return mass, diskr, eigv, ansatz, numb, matinpo
