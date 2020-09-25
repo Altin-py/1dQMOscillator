234cae527c7e0bbbbb3024b2563ab8f095eb740a
Cristina Oballe <cristinaoballe@gmail.com>
2020-09-25 14:26:50 +0200

    uncertainty.py corrected to include all eigenstates

--- uncertainty.py
+++ uncertainty.py
@@ -4,8 +4,7 @@ Calculates the uncertainty of a variable (multiplicative operator) in a 1D
 discretized quantum system.
 """
 import numpy as np
-NO HE TENIDO EN CUENTA QUE QUIERO CALCULARLO TODO PARA CADA POSICION Y PARA
-CADA AUTOESTADO
+
 
 def expectationval(aa, bb, delta, s, n):
     """Calculates the expectation value of the discretized operator in each
@@ -13,22 +12,24 @@ def expectationval(aa, bb, delta, s, n):
 
     Args:
         aa: Vector of the discretized values of the operator. Shape: (n,)
-        bb: Matrix of the eigenstates of the system. Shape: (n,s), where s is
+        bb: Matrix of the eigenstates of the system. Shape: (n, s), where s is
         the number of eigenstates.
         delta: Distance between the discretized points. Scalar.
         s: Number of eigenstates. Scalar.
         n: Number of discretized points. Scalar.
 
     Returns:
-        exp: Expectation value of the operator.
+        exp: Matrix of the expectation values of the operator in each position
+        (rows) and in each eigenstate (columns). Shape: (n, s)
 
     Raises:
 
     """
-    exp=0.0
-    for ii in range(n):
-        exp+=aa[ii]*bb[ii, s]
-    exp*=delta
+    exp=np.zeros((n, s))
+    for jj in range(s):
+        for ii in range(n):
+            exp[ii,jj]+=aa[ii]*bb[ii, jj]
+        exp[ii,jj]*=delta
     return exp
 
 
@@ -45,14 +46,15 @@ def uncertainty(aa, bb, delta, s, n):
         n: Number of discretized points. Scalar.
 
     Returns:
-        sigma: Uncertainty of the operator, defined as the square root of the
-        expectation value of the squared operator minus the squared expectation
-        value of the operator itself.
+        sigma: Matrix of the uncertainty values of the operator, defined as the
+        square root of the expectation value of the squared operator minus the
+        squared expectation value of the operator itself. Shape: (n, s)
 
     Raises:
 
     """
-    xx_2_in=expectationval(aa*aa, bb, delta, s, n)
-    xx_2_out=np.square(expectationval(aa, bb, delta, s, n))
-    sigma=np.sqrt(xx_2_in-xx_2_out)
+    xx_2=aa*aa
+    xx_2_exp=expectationval(xx_2, bb, delta, s, n)
+    xx_exp_2=np.square(expectationval(aa, bb, delta, s, n))
+    sigma=np.sqrt(xx_2_exp-xx_exp_2)
     return sigma
