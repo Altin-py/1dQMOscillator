14c61788abd559327e373c97b25783252430791f
Cristina Oballe <cristinaoballe@gmail.com>
2020-09-26 13:14:39 +0200

    Update of diagonalizer.py

--- diagonalizer.py
+++ diagonalizer.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 """Routines for the diagonalization of square matrices"""
-import numpy.linalg as linalg
+import scipy.linalg as linalg
 
 
 def diagonalize(aa):
-    """Diagonalizes a square matrix.
+    """Diagonalizes a complex hermitian or real symmetrical matrix.
 
     Args:
         aa: Matrix to be diagonalized. Shape: (n, n)
 
     Returns:
-        eigval: Vector (n,) with the unordered eigenvalues of the matrix aa.
-        eigvec: Matrix (n, n) whose column eigvec[:,i] correspond to the
+        eigval: Vector (s,) with the unordered eigenvalues of the matrix aa.
+        eigvec: Matrix (n, s) whose column eigvec[:,i] correspond to the
         eigenvector associated to the eigenvalue eigval[i].
 
     Raises:
         numpy.linalg.LinAlgError: if the eigenvalue computation does not
         converge.
     """
-    eigval, eigvec=linalg.eig(aa)
+    eigval, eigvec=linalg.eigh(aa)
     return eigval, eigvec
