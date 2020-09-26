fe10e57b637bcc4ca4199a76afa5a6b6e4843754
Altin Shala <s_b6kpo6@uni-bremen.de>
2020-09-26 14:21:45 +0200

    Optimized the pylint score of interpolate.py

--- interpolate.py
+++ interpolate.py
@@ -5,24 +5,24 @@ Reads out Data from reading.py and uses it to
 interpolate the potentials
 """
 import numpy as np
-import reading
 from scipy.interpolate import interp1d, CubicSpline, BarycentricInterpolator
+import reading
 def interpolate(directory):
     """
     Function to interpolate the data with chosen nethod
     """
     mass, diskr, eigv, ansatz, matinpo = reading.reading(directory)
 
-    xgraph = np.linspace(diskr[0], diskr[1], diskr[2])
+    xdat = np.linspace(diskr[0], diskr[1], diskr[2])
 
     if ansatz == 'linear':
-        yinter = interp1d(matinpo[:,0], matinpo[:,1], kind = 'linear')
-        ygraph = yinter(xgraph)
+        yinter = interp1d(matinpo[:, 0], matinpo[:, 1], kind='linear')
+        ypot = yinter(xdat)
     if ansatz == 'polynomial':
-        yinter = BarycentricInterpolator(matinpo[:,0], matinpo[:,1])
-        ygraph = yinter(xgraph)
+        yinter = BarycentricInterpolator(matinpo[:, 0], matinpo[:, 1])
+        ypot = yinter(xdat)
     if ansatz == 'cspline':
-        yinter = CubicSpline(matinpo[:,0], matinpo[:,1], bc_type='natural')
-        ygraph = yinter(xgraph)
+        yinter = CubicSpline(matinpo[:, 0], matinpo[:, 1], bc_type='natural')
+        ypot = yinter(xdat)
 
-    return xgraph, ygraph
+    return xdat, ypot
